fa545f33442513f2d0fcfe963a5e7f76901e336c
Yatin Parashar <parasheryatin@gmail.com>
2026-09-03 12:54:51 +0530

    Time: 7 ms (72.85%) | Memory: 22.3 MB (97.60%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.py
@@ -0,0 +1,13 @@
+class Solution:
+    def merge(self, intervals):
+        intervals.sort()
+
+        result = []
+
+        for interval in intervals:
+            if not result or interval[0] > result[-1][1]:
+                result.append(interval)
+            else:
+                result[-1][1] = max(result[-1][1], interval[1])
+
+        return result
